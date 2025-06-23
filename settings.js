5714e10921b137b42618e6ffab0e8bd216b089a0
paka235 <panelnew66@gmail.com>
2025-06-23 19:47:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐃ᴇᴠɪʟ-𝐓ᴇᴄʜ-𝐌ᴅ=DsZVQCTb#VqQiPoH3aOL8wiMwWMxErjWyLE6a98LN0Qd9RIN2Hhc' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'enter your session id' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
